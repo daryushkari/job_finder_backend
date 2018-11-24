58ff0325f087b9fc0ca2cd04c2fb4086d4b1bf44
daryushkari <daryushkari123@gmail.com>
2018-11-24 19:23:55 +0330

    Change database engine to djongo

--- job_finder/settings.py
+++ job_finder/settings.py
@@ -76,8 +76,8 @@ WSGI_APPLICATION = 'job_finder.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'djongo',
+        'NAME': 'job_finder_db',
     }
 }
 
